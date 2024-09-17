dbf526ee2a029e13f0b1ebbfa5e47626952e509b
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2024-09-17 15:43:37 +0500

    complete xylaphoneWithCode

--- XylophoneWithCode/XylophoneWithCode/ViewController.swift
+++ XylophoneWithCode/XylophoneWithCode/ViewController.swift
@@ -11,21 +11,92 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var player: AVAudioPlayer!
-
-       override func viewDidLoad() {
-           super.viewDidLoad()
-       }
-
-       @IBAction func keyPressd(_ sender: UIButton) {
-           playSound(sound: sender.currentTitle!)
-       }
-       
-       func playSound(sound: String) {
-           let url = Bundle.main.url(forResource: sound, withExtension: "wav")
-           player = try! AVAudioPlayer(contentsOf: url!)
-           player.play()
-       }
-
-
+    private let namedButtons = ["A", "C", "B", "F", "G", "E", "D"]
+    private var buttonStack = UIStackView()
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+        
+        setView()
+        setConstraints()
+        createButtons()
+    }
 }
 
+extension ViewController {
+    
+    private func toggleButtonAlpha(_ button: UIButton) {
+        button.alpha = button.alpha == 1 ? 0.5 : 1
+    }
+    
+    @objc private func keyPressed(_ sender: UIButton) {
+        toggleButtonAlpha(sender)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            self.toggleButtonAlpha(sender)
+        }
+        guard let buttonText = sender.currentTitle else { return }
+       playSound(sound: sender.currentTitle!)
+    }
+
+    private func playSound(sound: String) {
+       let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+       player = try! AVAudioPlayer(contentsOf: url!)
+       player.play()
+    }
+    
+    private func getColor(for name: String) -> UIColor {
+        return switch name {
+        case "A": .systemRed
+        case "C": .systemOrange
+        case "B" : .systemYellow
+        case "F": .systemGreen
+        case "G": .systemIndigo
+        case "E" : .systemBlue
+        case "D" : .systemPurple
+        default: .white
+        }
+    }
+    
+    private func setView(){
+        view.backgroundColor = .white
+        view.addSubview(buttonStack)
+        buttonStack.alignment = .center
+        buttonStack.translatesAutoresizingMaskIntoConstraints = false
+        buttonStack.backgroundColor = .green
+        buttonStack.axis = .vertical
+        buttonStack.spacing = 10
+        buttonStack.distribution = .fillEqually
+    }
+    
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            buttonStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            buttonStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttonStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            buttonStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)])
+    }
+    
+    private func createButtons(){
+        for (index, nameButton) in namedButtons.enumerated() {
+            let multipliedWidth = 0.97 - (0.03 * Double(index))
+            createButton(name: nameButton, width: multipliedWidth, background: getColor(for: nameButton))
+        }
+        //namedButton.forEach( (index, nameButton: String) in createButton(namedButton))
+    }
+    
+    private func createButton(name:String, width: Double, background:UIColor) {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(name, for: .normal)
+        button.backgroundColor = background
+        button.layer.cornerRadius = 10
+        button.tintColor = .white
+        button.titleLabel?.font = .systemFont(ofSize: 45)
+        button.addTarget(self, action: #selector(keyPressed), for: .touchUpInside)
+        buttonStack.addArrangedSubview(button)
+        button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: width).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        
+    }
+
+}

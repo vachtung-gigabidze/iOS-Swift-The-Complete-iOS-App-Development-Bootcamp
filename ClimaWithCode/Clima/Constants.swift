962abc51bbf85c912e86af115fdb63f8b8fc5b87
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2024-10-17 12:05:14 +0500

    Верстка Урок 20

---
+++ ClimaWithCode/Clima/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  Clima
+//
+//  Created by Дмирий Зядик on 17.10.2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static let background = "background"
+    static let weatherColor = "weatherColor"
+    static let celsius = "°C"
+    static let searchSF = "magnifyingglass"
+    static let geoSF = "location.circle.fill"
+    static let search = "Search"
+    static let coditionSF = "cloud.rain"
+}
---
+++ ClimaWithCode/Clima/UIKit + Extension.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+extension UIStackView {
+    convenience init(axis: NSLayoutConstraint.Axis, distribution: UIStackView.Distribution, alignment: UIStackView.Alignment, spacing: CGFloat, subViews: [UIView] ) {
+        self.init(arrangedSubviews: subViews)
+        self.axis = axis
+        self.distribution = distribution
+        self.spacing = spacing
+        self.alignment = alignment
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UILabel {
+    convenience init(text: String, textAlignment: NSTextAlignment) {
+        self.init()
+        self.text = text
+        self.textAlignment = textAlignment
+        self.font = .systemFont(ofSize: 30, weight: .bold)
+        self.textColor = .white
+        self.lineBreakMode = .byWordWrapping
+        self.numberOfLines = 0
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UIButton {
+    convenience init(text: String) {
+        self.init(type: .system)
+        
+        self.backgroundColor  =  UIColor(named: "backgroundColor")
+        self.tintColor = .white
+        
+        self.layer.cornerRadius = 20
+        self.layer.borderColor = UIColor(named: "buttonBorder")?.cgColor
+        self.layer.borderWidth = 5
+        self.titleLabel?.font = .systemFont(ofSize: 25, weight: .bold)
+        self.setTitle(text, for: .normal)
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}

a0c021343d1fa3d7190cbc7940c56585da54a68e
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2024-11-10 16:35:49 +0500

    258

---
+++ Todoey-iOS13/Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Дмирий Зядик on 09.11.2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    // MARK: - Add new Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    // MARK: -  TableView Datasource Methods
+    
+    // MARK: - TableView Delegate Methods
+    
+    // MARK: - Data Manipulation Methods
+}

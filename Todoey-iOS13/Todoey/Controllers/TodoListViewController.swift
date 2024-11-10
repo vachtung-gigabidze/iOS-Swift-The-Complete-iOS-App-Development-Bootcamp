3ba32c6d4db346cbbe037a06cdb37df7fb561a78
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2024-11-10 16:19:38 +0500

    257

--- Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -108,6 +108,17 @@ extension TodoListViewController: UISearchBarDelegate {
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         loadItems(with: request)
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
 }
 
 
